10fc1c134a949588d62f470bdffc028d780d0aac
RealCookieWizard <86242263+RealCookieWizard@users.noreply.github.com>
2025-04-28 21:01:44 -0600

    Add files via upload

---
+++ practicas/practica02/Consola.java
@@ -0,0 +1,7 @@
+package Reloj_con_mas_clases;
+
+public class Consola {
+    public static void imprimir(String mensaje) {
+        System.out.println(mensaje);
+}
+}
---
+++ practicas/practica02/CrearReloj.java
@@ -0,0 +1,7 @@
+package Reloj_con_mas_clases;
+
+public class CrearReloj {
+    public static Reloj crearRelojPredeterminado() {
+        return new Reloj(23, 59, 50);
+    }
+}
